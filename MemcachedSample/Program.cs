0ae933f6c13fbbf921668c5f8af43e00ee9ad581
hooshmandakbari <hooshmandakbari@outlook.com>
2024-05-05 15:29:41 +0330

    init

---
+++ MemcachedSample/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Enyim.Caching;
+using Enyim.Caching.Configuration;
+using Microsoft.Extensions.Logging;
+
+
+
+string key = "Age";
+
+// Create a logger factory configured to log to the console.
+using var loggerFactory = LoggerFactory.Create(builder =>
+    builder.AddConsole().SetMinimumLevel(LogLevel.Warning));
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+// Configure the Memcached client.
+var config = new MemcachedClientConfiguration(loggerFactory, new MemcachedClientOptions());
+config.AddServer("localhost", 11211); // Replace with your Memcached server address and port.
+
+// Create the Memcached client.
+MemcachedClient myCache = new MemcachedClient(loggerFactory, config);
+
+// Set a value in the cache.
+Console.WriteLine($"Setting - Key:{key}, Value:31");
+await myCache.SetAsync(key, 31, 10); // Expires after 10 seconds.
+
+// Wait for a second to allow the cache to be populated.
+Console.WriteLine("\nWaiting a second...");
+await Task.Delay(1000);
+
+// Get the value from the cache.
+int number = myCache.Get<int>(key);
+Console.WriteLine($"Getting - Key:{key}, Value:{number}");
